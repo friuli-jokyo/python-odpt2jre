513c0c5c2b6356e529eba4a3f508ce3fee3f1998
friuli-jokyo <26354730+friuli-jokyo@users.noreply.github.com>
2022-06-30 16:16:32 +0900

    use loops (without manual index number)

--- odpt2jre/text_format/rinkai.py
+++ odpt2jre/text_format/rinkai.py
@@ -14,28 +14,34 @@ def to_jre(info:odpt.TrainInformation) -> list[TrainInformation]:
     result:TrainInformation = TrainInformation(info)
     result.text_raw.ja = embed_field( info.train_information_text.ja )
 
-    info_text = result.text_raw.ja.split("。")
+    info_texts = result.text_raw.ja.split("。")
 
-    if len(info_text) >= 1 and (_match := re.fullmatch(r"(.+?)(の影響で|のため)(.+?)",info_text[0])):
-        info_text = [ _match[3], _match[1]+"の影響で" ] + info_text[1:]
-    if len(info_text) >= 1:
-        if info_text[0].startswith("運転を見合わせていましたが"):
+    # 「～の(影響で/のため)～」を分割
+    for i in range(len(info_texts)):
+        if (_match := re.fullmatch(r"(.+?)(の影響で|のため)(.+?)",info_texts[i])) and _match[3] != "す":
+            info_texts = info_texts[:i] + [ _match[3], _match[1]+"の影響で" ] + info_texts[i:]
+
+    for info_text in info_texts:
+        # 運転再開構文
+        if info_text.startswith("運転を見合わせていましたが"):
             result.status_occasion.enum = StatusEnum.OPERATION_RESUMED
-            for field in find_all_field(info_text[0]):
+            for field in find_all_field(info_text):
                 match field[0]:
                     case ClockTime.header:
                         result.time_resume = ClockTime(field[1])
 
-        if info_text[0].endswith("遅れがでています") or info_text[0].endswith("ダイヤが乱れています"):
+        # 運転状況
+        if info_text.endswith("遅れがでています") or info_text.endswith("ダイヤが乱れています"):
             result.status_main.enum = StatusEnum.DELAY
-        elif info_text[0].endswith("運転を見合わせています") and result.status_occasion.enum != StatusEnum.OPERATION_RESUMED:
+        elif info_text.endswith("運転を見合わせています") and result.status_occasion.enum != StatusEnum.OPERATION_RESUMED:
             result.status_main.enum = StatusEnum.OPERATION_STOP
-    if len(info_text) >= 3 and "なお、" in info_text[2]:
-        if info_text[2].endswith("一部列車に運休が出ています"):
+        elif info_text.endswith("一部列車に運休が出ています"):
             result.status_main.enum = StatusEnum.SOME_TRAIN_CANCEL
             result.status_main.modifiers[0].some_train = True
-    if len(info_text) >= 1:
-        if _match := re.search( f"({LineName.regex})との直通運転を(中止し|見合わせていましたが、直通運転を再開し)", info_text[0] ):
+
+    # 直通中止/再開はメインの状況がある場合サブに回す
+    for info_text in info_texts:
+        if _match := re.search( f"({LineName.regex})との直通運転を(中止し|見合わせていましたが、直通運転を再開し)", info_text ):
             if result.status_main.enum == StatusEnum.NULL:
                 if _match[3] == "中止し":
                     result.status_main.enum = StatusEnum.DIRECT_STOP
@@ -53,25 +59,26 @@ def to_jre(info:odpt.TrainInformation) -> list[TrainInformation]:
                 status.modifiers[0].lines.append(LineName(_match[1]))
                 result.sentences_sub.append( status )
 
+    # 原因
+    for info_text in info_texts:
+        if "影響" in info_text:
+            field_list = find_all_field(info_text)
 
-    if len(info_text) >= 2 and "影響" in info_text[1]:
-        field_list = find_all_field(info_text[1])
+            for field in field_list:
+                match field[0]:
+                    case CauseName.header:
+                        result.cause = Cause(field[1])
 
-        for field in field_list:
-            match field[0]:
-                case CauseName.header:
-                    result.cause = Cause(field[1])
-
-        for field in field_list:
-            match field[0]:
-                case LineName.header:
-                    if result.cause and field[1] != "[Line:TWR.Rinkai]":
-                        result.cause.lines.append(LineName(field[1]))
-                case SingleStation.header:
-                    if result.cause:
-                        result.cause.sections.append(SingleStation(field[1]))
-                case BetweenStations.header:
-                    if result.cause:
-                        result.cause.sections.append(BetweenStations(field[1]))
+            for field in field_list:
+                match field[0]:
+                    case LineName.header:
+                        if result.cause and field[1] != "[Line:TWR.Rinkai]":
+                            result.cause.lines.append(LineName(field[1]))
+                    case SingleStation.header:
+                        if result.cause:
+                            result.cause.sections.append(SingleStation(field[1]))
+                    case BetweenStations.header:
+                        if result.cause:
+                            result.cause.sections.append(BetweenStations(field[1]))
 
     return [result]
