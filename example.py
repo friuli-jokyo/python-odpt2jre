85765fe8e064e0193df87bdaa6ff4b44fe81f146
friuli-jokyo <26354730+friuli-jokyo@users.noreply.github.com>
2022-10-06 08:52:24 +0000

    avoid using import odpttraininfo

--- example.py
+++ example.py
@@ -1,14 +1,13 @@
-import odpttraininfo as odpt
 import odpt2jre
 
 # ODPT形式運行情報のキャッシュ保存先を指定(デフォルトは"./__odptcache__/")
-odpt.cache.set_cache_dir("./__odptcache__/")
+odpt2jre.set_odpt_cache_dir("./__odptcache__/")
 
 # JR東日本形式運行情報のキャッシュ保存先を指定(デフォルトは"./__jrecache__/")
-odpt2jre.cache.set_cache_dir("./__jrecache__/")
+odpt2jre.set_jre_cache_dir("./__jrecache__/")
 
 # 公共交通オープンデータセンターのconsumerKeyをセット(.env等を用いて環境変数"ODPT_CENTER_TOKEN"を設定すればここでセットする必要はありません)
-odpt.Distributor.ODPT_CENTER.set_consumer_key("xxxxxxxxxxxx")
+odpt2jre.Distributor.ODPT_CENTER.set_consumer_key("xxxxxxxxxxxx")
 
 # JR東日本形式運行情報のキャッシュを更新(内部でODPT形式運行情報のキャッシュ更新も行われています)
 odpt2jre.refresh_cache()
--- odpt2jre/__init__.py
+++ odpt2jre/__init__.py
@@ -1,8 +1,12 @@
+from odpttraininfo import Distributor
+from odpttraininfo.cache import set_cache_dir as set_odpt_cache_dir
+
 from . import debug
 from .cache import fetch_info, refresh_cache
+from .cache import set_cache_dir as set_jre_cache_dir
 from .intermediate_components import *
 from .intermediate_components.output_dict import list_diff
 
 __version__ = "0.0.1"
 
-__all__ = ["fetch_info","refresh_cache","debug","list_diff"]
+__all__ = ["fetch_info","refresh_cache","debug","list_diff","Distributor","set_jre_cache_dir","set_odpt_cache_dir"]
